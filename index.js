ce04fac4471821839b873a800270fb67c8e89063
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2022-09-19 11:13:58 +0300

    alert event listern

---
+++ index.js
@@ -0,0 +1,82 @@
+// example 1
+document.getElementById("react").addEventListener("click", function () {
+    alert("react developer");
+});
+
+// example 2
+document.getElementById("vue").addEventListener("click", function () {
+    alert("vue developer");
+});
+
+// example 3
+document.getElementById("angular").addEventListener("click", function () {
+    alert("angular developer");
+});
+
+// example 4
+document.getElementById("bootstrap").addEventListener("click", function () {
+    alert("bootstrap developer")
+});
+
+// example 5
+document.getElementById("deno").addEventListener("click", function () {
+    alert("deno developer");
+});
+
+// example 6
+document.getElementById("express").addEventListener("click", function () {
+    alert("express developer");
+});
+
+//example 7
+document.getElementById("adonis").addEventListener("click", function () {
+    alert("adonis developer");
+});
+
+// example 8
+document.getElementById("node").addEventListener("click", function () {
+    alert("node developer");
+})
+
+// example 10
+document.getElementById("rust").addEventListener("click", function () {
+    alert("rust developer");
+});
+
+// example 11
+document.getElementById("ruby").addEventListener("click", function () {
+    alert("ruby developer");
+});
+
+// example 12
+document.getElementById("django ").addEventListener("click", function () {
+    alert("django developer");
+});
+
+
+// example 13
+document.getElementById("django").addEventListener("click", function () {
+    alert("django developer");
+});
+
+
+// example 14
+document.getElementById("flask").addEventListener("click", function () {
+    alert("flask developer")
+});
+
+// example 15
+document.getElementById("flutter").addEventListener("click", function () {
+    alert("flutter developer")
+});
+
+// example 16
+document.getElementById("ios").addEventListener("click", function () {
+    alert("ios developer");
+});
+
+// example 17
+document.getElementById("react native").addEventListener("click", function () {
+    alert("react native debveloper")
+});
+
